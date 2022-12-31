f5c57133d9c52f9e321d1914054d4bc2a054e30f
xxx <ntzyq2021@163.com>
2022-12-31 19:04:56 +0800

    12.31 finish day2

---
+++ common/service_utils/src/main/java/com/atguigu/ggkt/result/Result.java
@@ -0,0 +1,133 @@
+package com.atguigu.ggkt.result;//package com.atguigu.ggkt.result;
+//
+//import lombok.Data;
+//
+//@Data
+//public class Result<T> {
+//
+//    private  Integer code;//状态码
+//
+//    private String message;//返回状态信息(成功失败)
+//
+//    private T data;//返回数据
+//
+//    public Result(){}
+//
+//    //成功的方法，没有data数据
+////    public static<T> Result<T> ok(){
+////        Result<T> result = new Result<>();
+////        result . setCode(200) ;
+////        result . setMessage("成功");
+////        return result;
+////    }
+//
+//    //失败的方法，没有data数据
+////    public static<T> Result<T> fail(){
+////        Result<T> result = new Result<>();
+////        result . setCode(201) ;
+////        result . setMessage("失败");
+////        return result;
+////    }
+//    //成功的方法，有data数据
+//    public static<T> Result<T> ok(T data){
+//        Result<T> result = new Result<>();
+//        if (data != null){
+//            result.setData(data);
+//        }
+//        result . setCode(200) ;
+//        result . setMessage("成功");
+//        return result;
+//    }
+//
+//    //失败的方法，有data数据
+//    public static<T> Result<T> fail(T data){
+//        Result<T> result = new Result<>();
+//        if (data != null){
+//            result.setData(data);
+//        }
+//        result . setCode(201) ;
+//        result . setMessage("失败");
+//        return result;
+//    }
+//
+//    public Result<T> message(String msg){
+//        this.setMessage(msg);
+//        return this;
+//    }
+//
+//    public Result<T> code(Integer code){
+//        this.setCode(code);
+//        return this;
+//    }
+//}
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 全局统一返回结果类
+ *
+ */
+@Data
+@ApiModel(value = "全局统一返回结果")
+public class Result<T> {
+
+    @ApiModelProperty(value = "返回码")
+    private Integer code;
+
+    @ApiModelProperty(value = "返回消息")
+    private String message;
+
+    @ApiModelProperty(value = "返回数据")
+    private T data;
+
+    public Result(){}
+
+    public static <T> Result<T> build(T body, Integer code, String message) {
+        Result<T> result = new Result<T>();
+        if (body != null) {
+            result.setData(body);
+        }
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
+    public static<T> Result<T> ok(){
+        return Result.ok(null);
+    }
+
+    /**
+     * 操作成功
+     * @param data  baseCategory1List
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> ok(T data){
+        return build(data,200,"成功");
+    }
+
+    public static<T> Result<T> fail(){
+        return Result.fail(null);
+    }
+
+    /**
+     * 操作失败
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> fail(T data){
+        return build(data, 201,"失败");
+    }
+
+    public Result<T> message(String msg){
+        this.setMessage(msg);
+        return this;
+    }
+
+    public Result<T> code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+}
--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -1,13 +1,20 @@
 package com.atguigu.ggkt.vod.controller;
 
 
+import com.atguigu.ggkt.exception.GgktException;
 import com.atguigu.ggkt.model.vod.Teacher;
+import com.atguigu.ggkt.result.Result;
+import com.atguigu.ggkt.vo.vod.TeacherQueryVo;
 import com.atguigu.ggkt.vod.service.TeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +26,8 @@ import java.util.List;
  * @author atguigu
  * @since 2022-12-30
  */
+
+@Api(tags = "讲师管理接口")
 @RestController
 @RequestMapping("/vod/teacher")
 public class TeacherController {
@@ -27,11 +36,103 @@ public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
-    //查询所有讲师列表
+    //http://localhost : 8301/vod/teacher/findAll
+    //1 查询所有讲师列表
+    @ApiOperation("查询所有讲师")
     @GetMapping("findAll")
-    public List<Teacher> findAll(){
+    public Result findAllTeacher() {
+        //模拟异常
+//        try {
+//            int a = 10/0;
+//        }catch(Exception e) {
+//            throw new GgktException(20001,"出现自定义异常");
+//        }
+
+        //调用service方法
         List<Teacher> list = teacherService.list();
-        return list;
+        return Result.ok(list).message("查询数据成功");
+    }
+
+    //2 逻辑删除讲师
+    @ApiOperation("逻辑删除讲师")
+    @DeleteMapping("remove/{id}")
+    public Result removeTeacher(@ApiParam(name = "id", value = "ID", required = true)
+                                @PathVariable Long id) {
+        boolean isSuccess = teacherService.removeById(id);
+        if (isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
+    //3 条件查询分页列表
+    @ApiOperation(value = "获取分页列表")
+    @PostMapping("{page}/{limit}")
+    public Result index(
+            @ApiParam(name = "page", value = "当前页码", required = true)
+            @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable Long limit,
+            @ApiParam(name = "teacherVo", value = "查询对象", required = false)
+            @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
+        //创建page对象，传递当前页和每页记录数
+        Page<Teacher> pageParam = new Page<>(page, limit);
+        //获取条件值
+        String name = teacherQueryVo.getName();//讲师名称
+        Integer level = teacherQueryVo.getLevel();//讲师级别
+        String joinDateBegin = teacherQueryVo.getJoinDateBegin();//开始时间
+        String joinDateEnd = teacherQueryVo.getJoinDateEnd();//结束时间
+        //封装条件
+        QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(joinDateBegin)) {
+            wrapper.ge("join_date", joinDateBegin);
+        }
+        if (!StringUtils.isEmpty(joinDateEnd)) {
+            wrapper.le("join_date", joinDateEnd);
+        }
+        //调用方法得到分页查询结果
+        IPage<Teacher> pageModel = teacherService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
+    }
+
+    //4 添加讲师
+    @ApiOperation("添加讲师")
+    @PostMapping("save")
+    public Result save(@RequestBody Teacher teacher) {
+        teacherService.save(teacher);
+        return Result.ok(null);
+    }
+
+    //5修改-根据id查询
+    @ApiOperation("根据id查询")
+    @GetMapping("getTeacher/{id}")
+    public Result getTeacher(@PathVariable Long id) {
+        Teacher teacher = teacherService.getById(id);
+        return Result.ok(teacher);
+    }
+
+    //6修改-最终实现
+    @ApiOperation(value = "修改")
+    @PutMapping("update")
+    public Result updateById(@RequestBody Teacher teacher) {
+        teacherService.updateById(teacher);
+        return Result.ok(null);
+    }
+
+    //7批量删除讲师
+    // json数组[1,2,3]
+    @ApiOperation(value = "根据id列表删除")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList) {
+        teacherService.removeByIds(idList);
+        return Result.ok(null);
     }
 }
 
